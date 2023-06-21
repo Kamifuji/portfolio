676df625bec258666cbe173906e3112bfe6df3d8
kamifuji.mayu <kamifuji.mayu@a-tm.co.jp>
2023-06-21 20:06:16 +0900

    記事ページへのリンクを追加

--- src/app/components/Articles/ArticleCard/ArticleCard.tsx
+++ src/app/components/Articles/ArticleCard/ArticleCard.tsx
@@ -1,18 +1,20 @@
-import Image from "next/image";
-import { styles } from "./ArticleCard.css";
+import Image from 'next/image'
+import { styles } from './ArticleCard.css'
+import Link from 'next/link'
 
 export const ArticleCard = () => {
   return (
-    <article>
-      <Image
-        src="/dummy_article.png"
-        alt="ダミー"
-        className={styles.image}
-        width={720}
-        height={539}
-        
-      />
-      <h2 className={styles.title}>ここにタイトルが入る</h2>
-    </article>
-  );
-};
+    <Link href="/hoge">
+      <article>
+        <Image
+          src="/dummy_article.png"
+          alt="ダミー"
+          className={styles.image}
+          width={720}
+          height={539}
+        />
+        <h2 className={styles.title}>ここにタイトルが入る</h2>
+      </article>
+    </Link>
+  )
+}
