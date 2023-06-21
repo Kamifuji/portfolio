7ef888ba3a2fa5d750b44fa3065c8a2558af86b7
kamifuji.mayu <kamifuji.mayu@a-tm.co.jp>
2023-06-21 19:24:58 +0900

    Topページにコンポーネントを配置

---
+++ src/app/page.css.ts
@@ -0,0 +1,12 @@
+import { style } from "@vanilla-extract/css";
+
+export const styles = {
+  main: style({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 100,
+    padding: 90,
+    background: 'white'
+  })
+}
