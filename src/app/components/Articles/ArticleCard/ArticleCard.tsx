d82d4f075f7535eaca09f3de6d0b241bdcc56bfa
kamifuji.mayu <kamifuji.mayu@a-tm.co.jp>
2023-07-12 20:33:25 +0900

    APIから取得した記事データを表示

--- src/app/components/Articles/ArticleCard/ArticleCard.tsx
+++ src/app/components/Articles/ArticleCard/ArticleCard.tsx
@@ -1,10 +1,18 @@
 import Image from 'next/image'
 import { styles } from './ArticleCard.css'
 import Link from 'next/link'
+import { FC } from 'react'
 
-export const ArticleCard = () => {
+type ArticleCardProps = {
+  title: string
+  href: string
+  date: string
+}
+
+export const ArticleCard: FC<ArticleCardProps> = (props) => {
+  const { title, href, date } = props
   return (
-    <Link href="/hoge">
+    <a href={href} target="_blank">
       <article>
         <Image
           src="/dummy_article.png"
@@ -13,8 +21,9 @@ export const ArticleCard = () => {
           width={720}
           height={539}
         />
-        <h2 className={styles.title}>ここにタイトルが入る</h2>
+        <h2 className={styles.title}>{title}</h2>
+        <p>{date}</p>
       </article>
-    </Link>
+    </a>
   )
 }
--- src/app/components/Articles/Articles.tsx
+++ src/app/components/Articles/Articles.tsx
@@ -1,13 +1,25 @@
+import { getQiitaPosts } from '@/app/api'
 import { ArticleCard } from './ArticleCard/ArticleCard'
 import { styles } from './Articles.css'
+import { cdate } from 'cdate'
+import { Item } from '../../../../qiita-types'
 
-export const Articles = () => {
+export const Articles = async () => {
+  const posts: Item[] = await getQiitaPosts()
+
   return (
     <section className={styles.section}>
-      <ArticleCard />
-      <ArticleCard />
-      <ArticleCard />
-      <ArticleCard />
+      {posts.map((post) => {
+        const formatDate = cdate(post.created_at).format('YYYY-MM-DD')
+        return (
+          <ArticleCard
+            key={post.id}
+            title={post.title}
+            date={formatDate}
+            href={post.url}
+          />
+        )
+      })}
     </section>
   )
 }
