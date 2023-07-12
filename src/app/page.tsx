e6814c5c94670c2740bc3bd5da67ffebc9dd9780
kamifuji.mayu <kamifuji.mayu@a-tm.co.jp>
2023-07-12 20:34:49 +0900

    非同期サーバーコンポーネントが型エラーになるためチェックから除外

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <KeyVisual />
+      {/* @ts-expect-error Server Component */}
       <Articles />
     </main>
   )
